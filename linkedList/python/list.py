4d5429d94f6e0abd29547dc9d4610dd46adb2feb
elcid14 <dev@localhost.localdomain>
2022-02-14 11:41:36 -0500

    added more method clean up to linked list/python

--- linkedList/python/list.py
+++ linkedList/python/list.py
@@ -18,23 +18,44 @@ class List:
         self.head = None #stores head of the list. Is None by default
 
     #Inserts new node into list. Make new node the new head and existiing head the next of new node
-
     def pushNode(self, newData):
         if(self.head == None):
             print("Linked list does not have head")
-        
-        print("Inserting New Node")
-        newNode = Node(newData)
-        newNode.next = self.head
-        self.head = newNode
+            newHead = Node(newData)
+            self.head = newHead
+            newHead.next = None
+        else:
+            print("Inserting New Node")
+            newNode = Node(newData)
+            newNode.next = self.head
+            self.head = newNode
 
+#adds node to tail
+    def pushNodeToTail(self, newData):
+        newTail = Node(newData)
+        temp = self.head
+        if(self.head == None):
+            self.head = newTail
+            self.head.next = None
+            return
+        while(temp.next != None):
+            temp = temp.next
+        temp.next = newTail
+        
 
     #removes node from any part of the linked list
+    def removeNode(self, node):
+        temp = self.head
+
 
 
     #removes head from the list
-
-
+    def removeHeadFromList(self):
+        temp = self.head
+        if(temp is not None):
+            self.head = temp.next
+            temp = None
+            return
 
     #removes tail from list
     def removeTail(self):
@@ -49,15 +70,6 @@ class List:
            print("No head in list")
        elif(self.head.next is None):
            self.head = None
-           
-
-
-
-# removes 
-
-
-
-
 
 
     #prints out all nodes in list. Simple while loop to itereate through list starting at head and call next if present
@@ -74,9 +86,9 @@ class List:
 if __name__ =='__main__':
     #create nodes
     nodeOne = Node({"message":"Head of List"})
-    nodeTwo = Node(2)
-    nodeThree = Node(3)
-    nodeFour = Node(4)
+    nodeTwo = Node({"message":"Node Two"})
+    nodeThree = Node({"message":"Node Three"})
+    nodeFour = Node({"message":"Node Four/ Tail"})
 
     #create linked list
 
@@ -90,17 +102,36 @@ if __name__ =='__main__':
     #list.printNodes()
 
     #test push method
-
+    print("PUSHING NODE TO HEAD---------------")
     newNodeTest = Node({"message":"New Node"})
     list.pushNode({"message":"New Node"})
     list.printNodes()
 
 
     # test delete nodes
-
+    print("REMOVING TAIL-----------------")
     list.removeTail()
     list.printNodes()
     listTwo = List()
     listTwo.removeTail()
 
-    
+
+    #test adding node as new tail
+    print("ADDING NEW NODE AS TAIL---------------")
+    list.pushNodeToTail({"message":"NEW Tail"})
+    list.printNodes()
+    print("ADDING NEW NODE AS TAIL TO LIST WITH NO HEAD---------------")
+    listTwo.pushNodeToTail({"message":"NEW Tail to list with no head"})
+    listTwo.printNodes()
+
+
+    #removes head from a list
+    print("REMOVING HEAD FROM LIST---------------")
+    list.removeHeadFromList()
+    list.printNodes()
+
+    #push node to empty list
+    print("PUSHING NODE TO HEAD OF EMPTY LIST---------------")
+    listThree = List()
+    listThree.pushNode({"message":"Head, First Node"})
+    listThree.printNodes()
