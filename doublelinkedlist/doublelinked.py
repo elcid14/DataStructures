5c309eab50b5fd446da6af9ba410be3e70ba471d
elcid14 <dev@localhost.localdomain>
2022-02-15 20:57:49 -0500

    further updates to linked list double

--- doublelinkedlist/doublelinked.py
+++ doublelinkedlist/doublelinked.py
@@ -40,7 +40,66 @@ class DoubleList:
             newNode.prev = self.head
             newNode.next = None
 
+    #pop head from double linked list
+    #if there is self.head and 
 
+    def popHead(self):
+        if self.head is None:
+            print("No head in list")
+            return
+        elif self.head.next is not None:
+            self.head = self.head.next
+            self.head.prev = None
+
+    #slice last node from the double linked list
+    def slice(self):
+        if self.head is None:
+            print("Empty list")
+            return
+        elif self.head.next is not None:
+            print("Slicing last node from double linked list")
+            head = self.head
+            while head.next is not None:
+                head = head.next
+            head.prev.next = None
+        else:
+            print("Slicing head from list")
+            if self.head.next is None:
+                self.head = None
+                return
+    #delete any nth node from the double linked list
+    def removeNode(self, node):
+       temp = self.head
+       #define bail out if none
+       if temp is None or node is None:
+           return
+        #delete if node is head
+       elif temp.data == node:
+           print("Deleting node that is head of list")
+           self.head = temp.next
+           self.head.prev = None
+        #delete if last node
+       elif temp.data == node and temp.next is None:
+           print(temp.prev.next)
+           #temp.next.prev = temp.prev   
+       else:
+           while temp:
+               if temp.data == node:
+                   temp.prev.next = temp.next
+                   temp.next.prev = temp.prev
+               temp = temp.next
+
+    def getLastNode(self):
+        head = self.head
+        if self.head.next is not None:
+            while head.next is not None:
+                head = head.next
+            print(head.prev.next.data)
+            
+
+
+
+        
     def printNodes(self):
         if(self.head == None):
             print("No starting Node") 
@@ -55,6 +114,31 @@ if __name__ == '__main__':
     dList = DoubleList()
     dList.insertNode(nodeOne.data)
     dList.printNodes()
+    print("INSERT NODES-----------------")
     dList.insertNode(2)
     dList.insertNode(3)
     dList.printNodes()
+    print("DELETE HEAD--------------")
+    dList.popHead()
+    dList.printNodes()
+    print("DELETE HEAD--------------")
+    dList.popHead()
+    dList.printNodes()
+    print("DELETE LAST NODE IN LIST----------")
+    dList.insertNode(1)
+    dList.insertNode(2)
+    dList.printNodes()
+    print("--------------------")
+    dList.slice()
+    dList.printNodes()
+    dList.insertNode(1)
+    dList.insertNode(2)
+    dList.insertNode("Test")
+    dList.insertNode("Test TWO")
+    dList.insertNode(5)
+    print("--------------------")
+    dList.printNodes()
+    print("REMOVING NODE FROM LIST AT ANY POS---------------------")
+    dList.getLastNode()
+    #dList.printNodes()
+    
